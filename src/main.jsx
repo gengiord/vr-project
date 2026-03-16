ce1f0ba2ed49ca6e328d83dd8a65821dcf4cb69b
ahovision <aho.vision@gmail.com>
2026-03-16 15:46:13 +0100

    Update vr-project submodule

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
+console.log('App version: 0.0.0 - build', new Date().toISOString())
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/vr-project">
